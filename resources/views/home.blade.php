a2df5c4a95a33db6f3a0f68d7010d4d5d110b502
MascarBone <simone.bonelli@gmail.com>
2021-11-03 18:00:50 +0100

    Displayed db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
 
-<pre>
-    {{-- {{var_dump($movies)}} --}}
-    <h1>testing</h1>
-    <h2>{{$movies[0]->title}}</h2>
-</pre>
+<div class="container">
+    <div class="row gy-3">
+        @foreach ($movies as $movie)
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header">{{$movie->title}}</div>
+                    <div class="card-body">
+                        <h6 class="card-title">{{$movie->original_title}}</h6>
+                        <p class="card-text">{{$movie->nationality}}</p>
+                        <p class="card-text">{{$movie->vote}}</p>
+                        <p class="card-text"><small class="text-muted">{{$movie->date}}</small></p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
     
     
 @endsection
